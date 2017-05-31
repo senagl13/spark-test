9aa6977f18e25e515a34b913c274093584003af8
senagl13 <gabriel.sena@e-hps.com>
2017-05-31 11:09:21 -0400

    spark-cass

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,6 +1,9 @@
 /**
   * Created by lausena on 5/31/17.
   */
+import com.datastax.spark.connector._
+import org.apache.spark.sql.cassandra._
+
 object Hello extends App {
   def sayHello(name: String) = "Hello, Scala!"
 }
